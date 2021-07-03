7fa61e730dbf4f0d97c1cc2d63b5d88b31783b4b
Piotr Sarnacki <drogus@gmail.com>
2021-07-03 18:10:35 +0200

    wip

--- Gemfile
+++ Gemfile
@@ -5,11 +5,11 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in simd-json.gemspec
 gemspec
 
-gem "rake", "~> 13.0"
+gem "rake"
 
-gem "minitest", "~> 5.0"
+gem "minitest"
 
-gem "rubocop", "~> 1.7"
+gem "rubocop"
 gem "benchmark-ips"
 gem "memory_profiler"
 gem 'rails'
--- src/lib.rs
+++ src/lib.rs
@@ -4,54 +4,13 @@ use mimalloc::MiMalloc;
 static GLOBAL: MiMalloc = MiMalloc;#[macro_use]
 
 extern crate rutie;
+use rutie::{Class, Object, RString, VM, Boolean, Hash, NilClass, Fixnum, Float, Array, AnyObject};
 
-use rutie::{Class, Object, RString, VM, Hash, NilClass, Fixnum, Float, Array, AnyObject};
+mod implementation;
 
-use simd_json;
-use serde_json::Value;
+use crate::implementation::rust_parse;
 
 class!(SimdJsonRust);
-
-fn value_to_object(json: &Value) -> AnyObject {
-    match json {
-        Value::Object(map) => {
-            let mut hash = Hash::new();
-            for (s, v) in map.iter() {
-                let rstring = RString::new_utf8(&s.clone());
-                hash.store(rstring, value_to_object(v));
-            }
-            hash.into()
-        }
-        Value::String(s) => RString::new(s).into(),
-        Value::Null => NilClass::new().into(),
-        Value::Array(vec) => {
-            let mut rarray = Array::new();
-            for v in vec.into_iter() {
-                rarray.push(value_to_object(&v));
-            }
-            rarray.into()
-        },
-        Value::Number(n) => {
-            if n.is_i64() {
-                match n.as_i64() {
-                    Some(n) => { Fixnum::new(n).into() },
-                    None => unreachable!()
-                }
-            } else if n.is_u64() {
-                match n.as_u64() {
-                    Some(n) => { Fixnum::new(n as i64).into() },
-                    None => unreachable!()
-                }
-            } else {
-                match n.as_f64() {
-                    Some(n) => { Float::new(n).into() },
-                    None => unreachable!()
-                }
-            }
-        },
-        Value::Bool(b) => rutie::Boolean::new(*b).into(),
-    }
-}
 
 methods!(
     SimdJsonRust,
@@ -62,10 +21,8 @@ methods!(
           map_err(|e| VM::raise_ex(e) ).
           unwrap();
 
-        let mut b = ruby_string.to_bytes_unchecked().to_vec();
-        let v: Value = simd_json::serde::from_slice(&mut b).unwrap();
-        let mut hash = Hash::new();
-        value_to_object(&v)
+
+        rust_parse(ruby_string)
     }
 );
 
